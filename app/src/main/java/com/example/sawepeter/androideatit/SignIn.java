5dbe6e9367eee4a4c244c18eb7616198fcd2a7f5
user.email <sawepeter6@gmail.com>
2018-05-10 21:06:36 +0300

    signin class

--- app/src/main/java/com/example/sawepeter/androideatit/SignIn.java
+++ app/src/main/java/com/example/sawepeter/androideatit/SignIn.java
@@ -2,8 +2,12 @@ package com.example.sawepeter.androideatit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignIn extends AppCompatActivity {
+    EditText edtPhone,edtPassword;
+    Button btnsigin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
