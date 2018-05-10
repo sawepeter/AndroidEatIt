bd9074b6938e79093244a114231ff74f33c9d3ca
user.email <sawepeter6@gmail.com>
2018-05-10 11:43:49 +0300

    mainactivity

--- app/src/main/java/com/example/sawepeter/androideatit/MainActivity.java
+++ app/src/main/java/com/example/sawepeter/androideatit/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.sawepeter.androideatit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnSignup;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnsignin);
+        btnSignup = (Button)findViewById(R.id.btnsignup);
     }
 }
 
