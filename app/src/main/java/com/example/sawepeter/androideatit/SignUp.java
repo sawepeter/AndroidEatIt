7466b7c90853be32932fb8b427592733511d5bdd
user.email <sawepeter6@gmail.com>
2018-05-11 12:51:45 +0300

    sign up class

--- app/src/main/java/com/example/sawepeter/androideatit/SignUp.java
+++ app/src/main/java/com/example/sawepeter/androideatit/SignUp.java
@@ -3,7 +3,11 @@ package com.example.sawepeter.androideatit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.rengwuxian.materialedittext.MaterialEditText;
+
 public class SignUp extends AppCompatActivity {
+
+    MaterialEditText edtphone,edtName,edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
