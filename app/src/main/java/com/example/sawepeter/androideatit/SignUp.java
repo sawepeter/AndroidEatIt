0c64d1ca8e935d7ddb4f0bad6b4a79bb97c36fd2
user.email <sawepeter6@gmail.com>
2018-05-11 12:53:47 +0300

    button method

--- app/src/main/java/com/example/sawepeter/androideatit/SignUp.java
+++ app/src/main/java/com/example/sawepeter/androideatit/SignUp.java
@@ -2,13 +2,14 @@ package com.example.sawepeter.androideatit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class SignUp extends AppCompatActivity {
 
     MaterialEditText edtphone,edtName,edtPassword;
-
+    Button btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
