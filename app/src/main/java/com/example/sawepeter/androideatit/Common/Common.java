ba20ef611c5ad8af173c20fe42944a7a749e9cae
user.email <sawepeter6@gmail.com>
2018-05-14 13:00:09 +0300

    model user

--- app/src/main/java/com/example/sawepeter/androideatit/Common/Common.java
+++ app/src/main/java/com/example/sawepeter/androideatit/Common/Common.java
@@ -1,8 +1,11 @@
 package com.example.sawepeter.androideatit.Common;
 
+import com.example.sawepeter.androideatit.Model.user;
+
 /**
  * Created by SAWE PETER on 5/14/2018.
  */
 
 public class Common {
+    public static user currentuser;
 }
