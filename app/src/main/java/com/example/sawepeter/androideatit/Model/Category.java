0cf83096ba5a43a5197ccec73fbce052c5ec9f73
user.email <sawepeter6@gmail.com>
2018-05-14 13:25:55 +0300

    setters and getters

--- app/src/main/java/com/example/sawepeter/androideatit/Model/Category.java
+++ app/src/main/java/com/example/sawepeter/androideatit/Model/Category.java
@@ -15,4 +15,20 @@ public class Category {
         Name = name;
         Image = image;
     }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
 }
