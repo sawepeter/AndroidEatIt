f44df3837c56e64a74e2bb1bd2cdf87869f1bdb9
user.email <sawepeter6@gmail.com>
2018-05-14 15:47:43 +0300

    View imports

--- app/src/main/java/com/example/sawepeter/androideatit/Interface/ItemClickListener.java
+++ app/src/main/java/com/example/sawepeter/androideatit/Interface/ItemClickListener.java
@@ -1,8 +1,11 @@
 package com.example.sawepeter.androideatit.Interface;
 
+import android.view.View;
+
 /**
  * Created by SAWE PETER on 5/14/2018.
  */
 
 public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
 }
