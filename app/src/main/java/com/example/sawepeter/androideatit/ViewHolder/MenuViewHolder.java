d1a4ebe6419478fb42e53bcd9f0d4ce921915a8d
user.email <sawepeter6@gmail.com>
2018-05-17 00:55:09 +0300

    constructor

--- app/src/main/java/com/example/sawepeter/androideatit/ViewHolder/MenuViewHolder.java
+++ app/src/main/java/com/example/sawepeter/androideatit/ViewHolder/MenuViewHolder.java
@@ -1,8 +1,19 @@
 package com.example.sawepeter.androideatit.ViewHolder;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 /**
  * Created by SAWE PETER on 5/17/2018.
  */
 
-public class MenuViewHolder {
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public MenuViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
