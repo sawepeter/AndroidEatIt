6252b8accbf9e00a948b631c5ae7baecf92becdb
user.email <sawepeter6@gmail.com>
2018-05-17 00:52:53 +0300

    viewholder

--- app/src/main/java/com/example/sawepeter/androideatit/ViewHolder/MenuViewHolder.java
+++ app/src/main/java/com/example/sawepeter/androideatit/ViewHolder/MenuViewHolder.java
@@ -1,39 +1,8 @@
 package com.example.sawepeter.androideatit.ViewHolder;
 
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.example.sawepeter.androideatit.Interface.ItemClickListener;
-
 /**
- * Created by SAWE PETER on 5/14/2018.
+ * Created by SAWE PETER on 5/17/2018.
  */
 
-public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-
-    public TextView txtMenuName;
-    public ImageView imageView;
-
-    private ItemClickListener itemClickListener;
-
-    public MenuViewHolder(View itemView) {
-        super(itemView);
-
-        txtMenuName = (TextView)itemView.findViewById(R.id.menu_name);
-        imageView = (ImageView)itemView.findViewById(R.id.imageView);
-
-        itemView.setOnClickListener(this);
-    }
-
-    public void setItemClickListener(ItemClickListener itemClickListener) {
-        this.itemClickListener = itemClickListener;
-    }
-
-    @Override
-    public void onClick(View v) {
-        itemClickListener.onClick(v,getAdapterPosition(),false);
-
-    }
+public class MenuViewHolder {
 }
