860a6699f40c2bdcad7fe0f6942b5873ce9a0e10
user.email <sawepeter6@gmail.com>
2018-05-17 00:56:53 +0300

    variables declaration

--- app/src/main/java/com/example/sawepeter/androideatit/ViewHolder/MenuViewHolder.java
+++ app/src/main/java/com/example/sawepeter/androideatit/ViewHolder/MenuViewHolder.java
@@ -2,12 +2,18 @@ package com.example.sawepeter.androideatit.ViewHolder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by SAWE PETER on 5/17/2018.
  */
 
 public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView txtMenuName;
+    public ImageView imageview;
+    
     public MenuViewHolder(View itemView) {
         super(itemView);
     }
