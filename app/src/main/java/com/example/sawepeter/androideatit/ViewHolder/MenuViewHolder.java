39b27d50213b3586c3b5b8579cc3281bc9975753
user.email <sawepeter6@gmail.com>
2018-05-17 01:03:28 +0300

    itemview

--- app/src/main/java/com/example/sawepeter/androideatit/ViewHolder/MenuViewHolder.java
+++ app/src/main/java/com/example/sawepeter/androideatit/ViewHolder/MenuViewHolder.java
@@ -5,6 +5,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.sawepeter.androideatit.R;
+
 /**
  * Created by SAWE PETER on 5/17/2018.
  */
@@ -13,9 +15,12 @@ public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnCl
 
     public TextView txtMenuName;
     public ImageView imageview;
-    
+
     public MenuViewHolder(View itemView) {
         super(itemView);
+
+        txtMenuName = (TextView)itemView.findViewById(R.id.menu_name);
+        imageview = (ImageView)itemView.findViewById(R.id.menu_image);
     }
 
     @Override
